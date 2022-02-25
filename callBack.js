91ab6aa9345eb97536464848cbab295486eac761
MsDroid <manoranjansngh77@gmail.com>
2022-02-25 12:47:12 +0530

    promises tutor

--- callBack.js
+++ callBack.js
@@ -1,10 +1,23 @@
-function create(data, callback) {
-    var str = "Hi this is data " + data
-    callback(str)
-}
+// function create(data, callback) {
+//     var str = "Hi this is data " + data
+//     callback(str)
+// }
 
-function logData(data) {
-    console.log(data)
-}
+// function logData(data) {
+//     console.log(data)
+// }
 
-create("Node is best", logData)
+// create("Node is best", logData)
+
+
+// ==============================
+// another callback example
+// ==============================
+
+const compute = (n1, n2, callingCallback) => callingCallback(n1, n2)
+
+const sum = (n1, n2) => n1 + n2
+const product = (n1, n2) => n1 * n2
+
+console.log(compute(12, 13, sum))
+console.log(compute(12, 13, product))
