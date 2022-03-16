8b253ccf78fb2bd75ca6cbaa457b3cd6a239f8e5
msdroid <manoranjansngh77@gmail.com>
2022-03-16 14:46:17 +0530

    multiple middleware

--- middleware.js
+++ middleware.js
@@ -2,14 +2,23 @@ const express = require('express')
 const app = express()
 
 
+// first middleware
 const myFunction = (req,res,next) => {
     const date = new Date().toLocaleString()
     console.log(date)
     next()
 }
 
+// second middleware
+const myFunction1 = (req,res,next) => {
+    const date = new Date().toLocaleString()
+    console.log(date)
+    next()
+}
 
-app.get('/middleware', myFunction, (req,res) => {
+
+// use multiple middleware
+app.get('/middleware', [myFunction,myFunction1], (req,res) => {
     res.send("hi middleware");
 })
 
